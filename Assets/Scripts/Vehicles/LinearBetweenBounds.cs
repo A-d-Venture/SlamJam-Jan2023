017a18db9bf3a6706ddca16ebff61fb0b331ec73
Anthony <aventu6@uic.edu>
2023-01-14 22:49:24 -0600

    simple forklift weapon added

---
+++ Assets/Scripts/Vehicles/LinearBetweenBounds.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LinearBetweenBounds : MonoBehaviour
+{
+    [SerializeField] GameObject upperBound;
+    [SerializeField] GameObject lowerBound;
+    [SerializeField] GameObject movingObject;
+
+    [SerializeField] private float upMoveSpeed = 5f;
+    [SerializeField] private float downMoveSpeed = 5f;
+
+    // Start is called before the first frame update
+    void Start()
+        {
+            
+        }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (Input.GetButton("Jump") && (Vector3.Distance(movingObject.transform.position, upperBound.transform.position) > 0))
+        {
+            movingObject.transform.position = Vector3.MoveTowards(movingObject.transform.position, upperBound.transform.position, upMoveSpeed * Time.deltaTime);
+        }
+        else
+        {
+            movingObject.transform.position = Vector3.MoveTowards(movingObject.transform.position, lowerBound.transform.position, downMoveSpeed * Time.deltaTime);
+        }
+    }
+}
