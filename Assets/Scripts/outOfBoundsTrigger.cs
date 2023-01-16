4b846d7d4522efa3a2a9950bdd7bcca1762156ac
Anthony <aventu6@uic.edu>
2023-01-15 22:36:21 -0600

    added win ui

--- Assets/Scripts/outOfBoundsTrigger.cs
+++ Assets/Scripts/outOfBoundsTrigger.cs
@@ -7,18 +7,22 @@ public class outOfBoundsTrigger : MonoBehaviour
     private bool Player1InTrigger = false;
     private bool Player2InTrigger = false;
 
+    [SerializeField] GameObject player1WinText;
+    [SerializeField] GameObject player2WinText;
+
     private void OnTriggerEnter(Collider other)
     {
         if (other.tag == "Player1" && Player1InTrigger == false && Player2InTrigger == false)
         {
             Debug.Log("Player 2 Wins!");
             Player1InTrigger = true;
+            player2WinText.SetActive(true);
 
         } else if (other.tag == "Player2" && Player2InTrigger == false && Player1InTrigger == false)
         {
             Debug.Log("Player 1 Wins!");
             Player2InTrigger = true;
-
+            player1WinText.SetActive(true);
         }
     }
 }
